cc5a3713bd91e39c7f01bb0941f5f971ae48296c
talimal <talimal@post.bgu.ac.il>
2021-03-08 13:45:20 +0200

    optimized runtime in startTirps

--- api.py
+++ api.py
@@ -8,17 +8,29 @@ CORS(app)
 def do_dict(child):
 	child.__dict__
 
-@app.route('/data', methods=['GET'])
-def get_data():
+@app.route('/startData', methods=['GET'])
+def get_start_data():
 	manager = main.start_project()
 	start_dict = manager.get_start_dictionary()
-	jsonFinal={}
+	jsonStart={}
 	for entry in start_dict:
 		array_children = []
 		for child in start_dict[entry]:
 			array_children.append(json.dumps(child.__dict__))
-		jsonFinal[json.dumps(entry.__dict__)]=array_children
-	return jsonFinal
+		jsonStart[json.dumps(entry.__dict__)]=array_children
+	return jsonStart
+
+@app.route('/endData', methods=['GET'])
+def get_end_data():
+	manager = main.start_project()
+	end_dict = manager.get_end_dictionary()
+	jsonEnd={}
+	for entry in end_dict:
+		array_children = []
+		for child in end_dict[entry]:
+			array_children.append(json.dumps(child.__dict__))
+		jsonEnd[json.dumps(entry.__dict__)]=array_children
+	return jsonEnd
 
 
 @app.route('/')
--- maps_manager.py
+++ maps_manager.py
@@ -7,11 +7,11 @@ class maps_manager (object):
     def __init__(self, tirps):
         self.start_dictionary = dict()
         self.come_before = dict()
-        # self.end_dictionary = dict()
+        self.end_dictionary = dict()
         self.tirps = tirps
         self.init_dictionaries()
         self.build_start_dictionary()
-        # self.build_end_dictionary()
+        self.build_end_dictionary()
 
 
 
@@ -23,15 +23,15 @@ class maps_manager (object):
         return self.come_before
 
     """returns the dictionary- key[TIRP]:value - list of TIRPS that come before it"""
-    # def get_end_dictionary(self):
-    #     return self.end_dictionary
+    def get_end_dictionary(self):
+        return self.end_dictionary
 
     """initiates location for each TIRP in every dictionary"""
     def init_dictionaries(self):
         for tirp in self.tirps:
             self.start_dictionary[tirp] = list()
             self.come_before[tirp] = list()
-            # self.end_dictionary[tirp] = list()
+            self.end_dictionary[tirp] = list()
 
     """returns all the TIRPS in the start_dic that are in the specific size"""
     def get_start_tirps_by_size(self, tirp_size):
@@ -42,22 +42,45 @@ class maps_manager (object):
         return tirps
 
     """returns all the TIRPS in the end_dic that are in the specific size"""
-    # def get_end_tirps_by_size(self, tirp_size):
-    #     tirps = list()
-    #     for tirp in self.end_dictionary:
-    #         if tirp.get_size() == tirp_size:
-    #             tirps.append(tirp)
-    #     return tirps
+    def get_end_tirps_by_size(self, tirp_size):
+        tirps = list()
+        for tirp in self.end_dictionary:
+            if tirp.get_size() == tirp_size:
+                tirps.append(tirp)
+        return tirps
 
     """returns all the TIRPS that start in specific TIRP (come after it)"""
     def get_starts_by_tirp(self, tirp):
         return self.start_dictionary[tirp]
 
     """returns all the TIRPS that end in specific TIRP (come before it)"""
-    # def get_ends_by_tirp(self, tirp):
-    #     return self.end_dictionary[tirp]
+    def get_ends_by_tirp(self, tirp):
+        return self.end_dictionary[tirp]
 
     """created the start_dic : for each TIRP adds it to the right location according to it's size, symbols, relations"""
+    # def build_start_dictionary2(self):
+    #     for tirp in self.tirps:
+    #         # if tirp size if 1, there is no tirps from left(before it), then continue
+    #         tirp_size = tirp.get_size()
+    #         if tirp_size == 1:
+    #             continue
+    #
+    #         # for every risha (רישא) of the TIRP, we want to add this TIRP to the right of the risha
+    #         # n is the size of the risha:
+    #         for n in range(1,tirp_size):
+    #             #get all the TIRPs in size of risha
+    #             tirps_in_size_n = self.get_start_tirps_by_size(tirp_size=n)
+    #             #get all the TIRPs in size of risha with the same symbols as the current TIRP
+    #             risha_symbols = tirp.get_symbols()[0:n]
+    #             tirps_with_symbols = self.get_tirps_by_symbols(tirps_list=tirps_in_size_n, symbol_list=risha_symbols)
+    #             #from all the TIRPs till now, get the ones with the same relations as the current TIRP
+    #             num_relations = int((n)*(n-1)/2)
+    #             risha_relations = tirp.get_relations()[0:num_relations]
+    #             risha_tirps = self.get_tirps_by_relations(tirps_list=tirps_with_symbols, relation_list=risha_relations)
+    #             #add the current TIRP from the right to all the risha TIRPs
+    #             self.add_tirp_to_start_by_tirplist(tirps_list=risha_tirps, tirp_to_add=tirp)
+    #             self.add_before_tirps_by_tirp(tirp=tirp, before_tirp_list=risha_tirps)
+
     def build_start_dictionary(self):
         for tirp in self.tirps:
             # if tirp size if 1, there is no tirps from left(before it), then continue
@@ -65,21 +88,24 @@ class maps_manager (object):
             if tirp_size == 1:
                 continue
 
-            # for every risha (רישא) of the TIRP, we want to add this TIRP to the right of the risha
             # n is the size of the risha:
-            for n in range(1,tirp_size):
-                #get all the TIRPs in size of risha
-                tirps_in_size_n = self.get_start_tirps_by_size(tirp_size=n)
-                #get all the TIRPs in size of risha with the same symbols as the current TIRP
-                risha_symbols = tirp.get_symbols()[0:n]
-                tirps_with_symbols = self.get_tirps_by_symbols(tirps_list=tirps_in_size_n, symbol_list=risha_symbols)
-                #from all the TIRPs till now, get the ones with the same relations as the current TIRP
-                num_relations = int((n)*(n-1)/2)
+
+            n=tirp_size-1
+            #get all the TIRPs in size of risha
+            tirps_in_size_n = self.get_start_tirps_by_size(tirp_size=n)
+            #get all the TIRPs in size of risha with the same symbols as the current TIRP
+            risha_symbols = tirp.get_symbols()[0:n-1]
+            tirps_with_symbols = self.get_tirps_by_symbols(tirps_list=tirps_in_size_n, symbol_list=risha_symbols)
+            #from all the TIRPs till now, get the ones with the same relations as the current TIRP
+            num_relations = int((n)*(n-1)/2) # n=size-1
+            risha_relations=[]
+            if num_relations>0:
                 risha_relations = tirp.get_relations()[0:num_relations]
-                risha_tirps = self.get_tirps_by_relations(tirps_list=tirps_with_symbols, relation_list=risha_relations)
-                #add the current TIRP from the right to all the risha TIRPs
-                self.add_tirp_to_start_by_tirplist(tirps_list=risha_tirps, tirp_to_add=tirp)
-                self.add_before_tirps_by_tirp(tirp=tirp, before_tirp_list=risha_tirps)
+
+            risha_tirps = self.get_tirps_by_relations(tirps_list=tirps_with_symbols, relation_list=risha_relations)
+            #add the current TIRP from the right to all the risha TIRPs
+            self.add_tirp_to_start_by_tirplist(tirps_list=risha_tirps, tirp_to_add=tirp)
+            self.add_before_tirps_by_tirp(tirp=tirp, before_tirp_list=risha_tirps)
 
     """return all the tirps in the list that has the same symbol list as given"""
     def get_tirps_by_symbols(self, tirps_list, symbol_list):
@@ -111,8 +137,22 @@ class maps_manager (object):
 
 
     """for every tirp in start_dic, all it's values-big tirps, add this tirp to their end_dic"""
-    # def build_end_dictionary(self):
-    #     for tirp in self.start_dictionary:
-    #         start_tirp_list = self.start_dictionary[tirp]
-    #         for big_tirp in start_tirp_list:
-    #             self.end_dictionary[big_tirp].append(tirp)
+    def build_end_dictionary(self):
+     for tirp in self.start_dictionary:
+         tirp_size = tirp.get_size()
+         n=tirp_size
+         end_tirp_size=tirp_size-1
+         if end_tirp_size==0:
+             continue
+         else:
+             smaller_end_tirps = self.get_start_tirps_by_size(tirp_size=end_tirp_size)
+             symbol_list = tirp.get_symbols()[1:n]
+             tirps_symbols = self.get_tirps_by_symbols(tirps_list=smaller_end_tirps,symbol_list=symbol_list)
+             number_relations = int((n-1)*(n-2)/2)
+             sifa_relations = []
+             if number_relations>0:
+                sifa_relations = tirp.get_relations()[n-number_relations:n]
+             tirps_relations = self.get_tirps_by_relations(tirps_list=tirps_symbols,relation_list=sifa_relations)
+             for sifa_tirp in tirps_relations:
+                 self.end_dictionary[sifa_tirp].append(tirp)
+
